3a92b140d6646b22f9d94b0d8912e384dc2d50dd
doxizon <168129211+doxizon@users.noreply.github.com>
2024-06-10 15:35:13 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
+function openFullscreen(elem) {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+    }
+}
 window.onload = function() {
-    var div = document.getElementById('score');
-    
-    if(localStorage.getItem('divValue')) {
-      div.innerHTML = localStorage.getItem('divValue');
-    }
-  
-    div.oninput = function() {
-      localStorage.setItem('divValue', div.innerHTML);
-    }
-  }
+    openFullscreen(document.documentElement);
+}
+
 const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
     manifestUrl: 'https://doxizon.github.io/tonconnect-manifest.json',
     buttonRootId: 'ton-connect'
@@ -17,16 +21,18 @@ async function connectToWallet() {
  const connectedWallet = await tonConnectUI.connectWallet();
     console.log(connectedWallet);
 }
-var isOriginal = true;
-document.getElementById("coin").addEventListener("click", async function () {
-    document.getElementById("score").innerText++;
-})
+// var isOriginal = true;
+// document.getElementById("coin").addEventListener("click", async function () {
+//     document.getElementById("score").innerText++;
+// })
 
 document.getElementById("coin-img").addEventListener('touchstart', function() {
-    this.src = 'imgs/coin_press.png'; // замените на путь к вашему новому изображению
+    this.src = 'imgs/coin_press.png'; 
+    document.getElementById("score").innerText++;
+    // this.style.transform="rotate"
 });
 document.getElementById("coin-img").addEventListener('touchend', function() {
-    this.src = 'imgs/coin.png'; // замените на путь к вашему оригинальному изображению
+    this.src = 'imgs/coin.png'; 
 });
 
 
@@ -57,4 +63,3 @@ async function sendtrans(to, value){
 document.getElementById("home").addEventListener("click", async function() {
     await tonConnectUI.disconnect();
 });
-// 60
