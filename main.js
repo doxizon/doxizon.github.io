bbd524e26a0d7e494383a4d906eeb85377f4fbfd
doxizon <168129211+doxizon@users.noreply.github.com>
2024-06-10 07:34:26 +0300

    main

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+window.onload = function() {
+    var div = document.getElementById('score');
+    
+    if(localStorage.getItem('divValue')) {
+      div.innerHTML = localStorage.getItem('divValue');
+    }
+  
+    div.oninput = function() {
+      localStorage.setItem('divValue', div.innerHTML);
+    }
+  }
 const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
     manifestUrl: 'https://doxizon.github.io/tonconnect-manifest.json',
     buttonRootId: 'ton-connect'
@@ -6,9 +17,19 @@ async function connectToWallet() {
  const connectedWallet = await tonConnectUI.connectWallet();
     console.log(connectedWallet);
 }
+var isOriginal = true;
 document.getElementById("coin").addEventListener("click", async function () {
     document.getElementById("score").innerText++;
 })
+
+document.getElementById("coin-img").addEventListener('touchstart', function() {
+    this.src = 'imgs/coin_press.png'; // замените на путь к вашему новому изображению
+});
+document.getElementById("coin-img").addEventListener('touchend', function() {
+    this.src = 'imgs/coin.png'; // замените на путь к вашему оригинальному изображению
+});
+
+
 function nanoton(tons) {
     return tons * Math.pow(10, 9);
 }
@@ -18,7 +39,7 @@ async function sendtrans(to, value){
         validUntil: Math.floor(Date.now() / 1000) + 60, 
         messages: [
             {
-                address: "UQATnIXNmdRCU86YB9CllVd47_GfY5iaYBL7Zoobu4MuGBKM",
+                address: "UQBeXw_in5Sjn8iIkDLz_YFpPuvMc4SWRinEu9kDuK-uE_uS",
                 amount: nanoton(0.01),
                 // payload: "base64Hello"
             }
